21dbd3620b9003c9e07f82e1863f4b286ffe9af1
Felicitous1 <afoyusuf@gmail.com>
2021-08-31 20:56:40 +1000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,9 +37,6 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
-                        Our Offerings
-                      </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
